be5985508b0867491374ec58d007b3c8695d455a
zhanlinsong <zhanlscq@163.com>
2020-05-18 13:44:59 +0800

    init project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import noteService from './services/notes'
+import Notification from './components/Notification'
+import * as serviceWorker from './serviceWorker'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [errMsg, setErrMsg] = useState(null)
+  // const [showAll, setShowAll] = useState(true)
+  // 默认设定每次渲染完成都会调用，第二个参数传[]表示只执行一次
+  useEffect(() => {
+    noteService.getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    noteService.update(id, changedNote)
+      .then(returnedNote => {
+        console.log('importance of ' + id + ' has been toggled to ' + returnedNote.important)
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
+      .catch(err => {
+        setErrMsg(`Note '${note.content}' was already removed from server`)
+        setTimeout(() => {
+          setErrMsg(null)
+        }, 3000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const addNote = e => {
+    e.preventDefault()
+    const newNoteObject = {
+      "id": notes.length + 1,
+      "content": newNote,
+      "date": new Date().toISOString(),
+      "important": Math.random() > 0.5
+    }
+    noteService.create(newNoteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const deleteNoteOf = id => {
+    noteService.remove(id)
+      .then(() => {
+        setNotes(notes.filter(v => v.id !== id))
+      })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errMsg}/>
+      <ul>
+        {notes.map(
+          v => 
+            <li className="note" key={v.id}>
+              <span>{v.content}</span>
+              <button onClick={() => toggleImportanceOf(v.id)}>{v.important ? "no" : "yes"}</button>
+              <button onClick={() => deleteNoteOf(v.id)}>remove</button>
+            </li>
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={e => setNewNote(e.target.value)}></input>
+        <button type="submit">add</button>
+      </form>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/services/notes.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const REST_notes = 'http://localhost:3001/api/notes'
+
+const getAll = () => {
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  }
+  return axios.get(REST_notes).then(res => res.data.concat(nonExisting))
+}
+
+const create = newObject => {
+  return axios.post(REST_notes, newObject).then(res => res.data)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${REST_notes}/${id}`, newObject).then(res => res.data)
+}
+
+const remove = id => {
+  return axios.delete(`${REST_notes}/${id}`).then(res => res.data)
+}
+
+export default { getAll, create, update, remove }
